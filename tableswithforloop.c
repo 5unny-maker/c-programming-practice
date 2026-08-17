a9458e99cf45257a2802c7c6d1b68268fd304e32
5unny-maker <yondogaming@gmail.com>
2026-08-17 17:31:43 +0530

    Add files via upload

---
+++ tableswithforloop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+int N,i,result;
+scanf ("%d",&N);
+for(i=1;i<=10;i++)
+{
+    result=N*i;
+    printf("%d x %d = %d\n",N,i,result);
+   
+  
+}
+
+}
